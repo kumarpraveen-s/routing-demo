e9fb7af66bef7e27a86e0456e79a84afb7604bfa
Praveenkumar S <kumarpraveen-s>
2023-01-15 13:25:41 +0530

    Adding command

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Selects the div element from id in index.html present in public folder
+// Render the App component in it
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
